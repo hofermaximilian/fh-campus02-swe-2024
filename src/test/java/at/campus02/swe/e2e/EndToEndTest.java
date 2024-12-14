ee320004c3dc7dd965b2a50566ea762b20fd6c0f
MarcoHugeri <11840816@edu.campus02.at>
2024-12-14 14:36:46 +0100

    Added Solution for Issue #9 implemented sin/cos + 2 Unit Tests + 2 E2E Tests

--- src/test/java/at/campus02/swe/e2e/EndToEndTest.java
+++ src/test/java/at/campus02/swe/e2e/EndToEndTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 public class EndToEndTest {
 
     @Test
-    public void Test04Xml() throws Exception {
+    public void MulTest() throws Exception {
 
         Calculator cal = new CalculatorImpl();
 
@@ -22,11 +22,31 @@ public class EndToEndTest {
     }
 
     @Test
-    public void Test05Xml() throws Exception {
+    public void ModTest() throws Exception {
 
         Calculator cal = new CalculatorImpl();
 
         Parser parser = new Parser(cal);
         Assert.assertEquals(2.0,parser.parse(new File("src/test/resources/test05-e2e.xml")),0);
     }
+
+    @Test
+    public void SinTest() throws Exception {
+
+        Calculator cal = new CalculatorImpl();
+
+        Parser parser = new Parser(cal);
+        Assert.assertEquals(1,parser.parse(new File("src/test/resources/test06-e2e.xml")),0);
+    }
+
+    @Test
+    public void CosTest() throws Exception {
+
+        Calculator cal = new CalculatorImpl();
+
+        Parser parser = new Parser(cal);
+        Assert.assertEquals(-1,parser.parse(new File("src/test/resources/test07-e2e.xml")),0);
+    }
+
+
 }
--- src/test/java/at/campus02/swe/parser/ParserTest.java
+++ src/test/java/at/campus02/swe/parser/ParserTest.java
@@ -40,4 +40,6 @@ public class ParserTest {
 
         verifyNoMoreInteractions(cal);
     }
+
+
 }
